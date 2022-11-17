b9ed74305bcbfae3cbc2ab2074b5d4214f57c0e0
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-11-17 18:45:35 +0200

    created component

---
+++ src/App/App.js
@@ -0,0 +1,10 @@
+import {Text} from "./App.styled";
+function App() {
+	return (
+		<>
+			<Text>lkdjgl;djglkjdklgdlvldfjlkjdgldjgsdgjlkdjf;glkjdslkfjgldfg;lkjdlfgjdf;lkjglkdfgldkfj</Text>
+		</>
+	);
+}
+
+export default App;
