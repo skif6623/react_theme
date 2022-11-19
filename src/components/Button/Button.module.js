aeb3e3f727c6147c29669c9c658aefa383ee323c
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-11-19 16:58:34 +0200

    add style for buttons

--- src/components/Button/Button.module.js
+++ src/components/Button/Button.module.js
@@ -6,5 +6,14 @@ export const ButtonStyled = styled.button`
   padding-bottom: ${p => p.theme.space[3]}px;
   padding-left: ${p => p.theme.space[4]}px;
   padding-right: ${p => p.theme.space[4]}px;
+  border: ${p => p.theme.borders.normal};
+  border-radius: ${p => p.theme.radii.normal};
+  background-color: ${p => p.theme.colors.primary};
+  color: ${p => p.theme.colors.muted};
   cursor: pointer;
+
+  :hover,
+  :focus {
+    background-color: ${p => p.theme.colors.secondary};
+  }
 `;
