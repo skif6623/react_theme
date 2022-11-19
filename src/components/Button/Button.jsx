289a2634ce088d21118ea5deba9224a05ff56dd4
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-11-19 18:31:00 +0200

    added icons and deleted

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,10 @@
 import { ButtonStyled } from './Button.module';
+import { IoAddCircleOutline } from 'react-icons/io5';
 
 export const Button = ({ type = 'button', children, disabled = false }) => {
   return (
     <ButtonStyled disabled={disabled} type={type}>
+      <IoAddCircleOutline />
       {children}
     </ButtonStyled>
   );
--- src/components/Button/Button.module.js
+++ src/components/Button/Button.module.js
@@ -1,6 +1,9 @@
 import styled from '@emotion/styled';
 
 export const ButtonStyled = styled.button`
+  display: inline-flex;
+  align-items: center;
+  gap: ${p => p.theme.space[2]}px;
   margin: ${p => p.theme.space[0]};
   padding-top: ${p => p.theme.space[3]}px;
   padding-bottom: ${p => p.theme.space[3]}px;
